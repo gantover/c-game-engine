1350f39bb4d5406920b44eebd64463157a8e4168
gantover <gantoverpro@gmail.com>
2023-09-01 01:05:58 +0200

    applying transformation on vertexes only && 2 cubes obj test

--- main.c
+++ main.c
@@ -5,6 +5,7 @@
 #include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define res 1
 #define SW 160 * res // Screen width
@@ -44,8 +45,8 @@ typedef struct {
 } fvec3d;
 
 typedef struct {
-  fvec3d *v[3];
-} ftriangle; // triangle
+  int ind[3];
+} ftriangle;
 
 typedef struct {
   int x, y;
@@ -54,6 +55,8 @@ typedef struct {
 typedef struct {
   ftriangle *faces;
   int numFaces;
+  fvec3d *vertexes;
+  int numVertex;
 } mesh;
 
 mesh Cube;
@@ -83,12 +86,12 @@ void moveVec(fvec3d *vec) { // from player position
   (*vec).z -= P.z;
 }
 
-void multMatVec(fvec3d *i, fvec3d *o, mat4 mat) {
-  o->x = i->x * mat[0][0] + i->y * mat[1][0] + i->z * mat[2][0] + mat[3][0];
-  o->y = i->x * mat[0][1] + i->y * mat[1][1] + i->z * mat[2][1] + mat[3][1];
-  o->z = i->x * mat[0][2] + i->y * mat[1][2] + i->z * mat[2][2] + mat[3][2];
+void multMatVec(fvec3d *i, fvec3d *o, mat4 *mat) {
+  o->x = i->x * (*mat)[0][0] + i->y * (*mat)[1][0] + i->z * (*mat)[2][0] + (*mat)[3][0];
+  o->y = i->x * (*mat)[0][1] + i->y * (*mat)[1][1] + i->z * (*mat)[2][1] + (*mat)[3][1];
+  o->z = i->x * (*mat)[0][2] + i->y * (*mat)[1][2] + i->z * (*mat)[2][2] + (*mat)[3][2];
   float w =
-      i->x * mat[0][3] + i->y * mat[1][3] + i->z * mat[2][3] + mat[3][3];
+      i->x * (*mat)[0][3] + i->y * (*mat)[1][3] + i->z * (*mat)[2][3] + (*mat)[3][3];
   if (w != 0.0) {
     *o = (fvec3d){o->x / w, o->y / w, o->z};
   }
@@ -100,7 +103,7 @@ void scaleVec(fvec3d *v) { // we dont take in and out since we can modify this v
 }
 
 void readObj() {
-  FILE *fp = fopen("cube.obj", "r");
+  FILE *fp = fopen("twocubes.obj", "r");
   char *line = NULL;
   size_t len = 0;
   ssize_t read;
@@ -123,12 +126,14 @@ void readObj() {
   int ind[3];
   for (int j = 0; j < numFaces; j++) {
     fscanf(fp, "%i %i %i", &ind[0], &ind[1], &ind[2]);
-    faces[j].v[0] = &vertexes[ind[0] - 1];
-    faces[j].v[1] = &vertexes[ind[1] - 1];
-    faces[j].v[2] = &vertexes[ind[2] - 1];
+    faces[j].ind[0]=ind[0]-1;
+    faces[j].ind[1]=ind[1]-1;
+    faces[j].ind[2]=ind[2]-1;
   }
   Cube.faces = faces;
   Cube.numFaces=numFaces;
+  Cube.vertexes=vertexes;
+  Cube.numVertex=numVertex;
   printf("------ end reading file ------\n");
   fclose(fp);
 }
@@ -165,19 +170,26 @@ void drawTriangle(fvec3d *v0, fvec3d *v1, fvec3d *v2) {
 }
 
 void draw3D() {
-  fvec3d o0, o1, o2;
-  for (int i = 0; i < Cube.numFaces; i++) {
-    ftriangle face = Cube.faces[i];
-    fvec3d v0=*face.v[0], v1=*face.v[1], v2=*face.v[2];
-    moveVec(&v0); moveVec(&v1); moveVec(&v2);
-    multMatVec(&v0, &o0, projMat);
-    multMatVec(&v1, &o1, projMat);
-    multMatVec(&v2, &o2, projMat);
-    scaleVec(&o0);
-    scaleVec(&o1);
-    scaleVec(&o2);
-    drawTriangle(&o0, &o1, &o2);
+  fvec3d *vertexes = malloc(sizeof(fvec3d)*Cube.numVertex);// cube copy (to not alter original cooridinates)
+  for (int i = 0; i<Cube.numVertex; i++) {
+    vertexes[i] = Cube.vertexes[i]; // have to assign each array element, the pointer to an array only points to its first element
   }
+  for (int i = 0; i<Cube.numVertex; i++) {
+    fvec3d *v = &(vertexes[i]);
+    moveVec(v);
+    fvec3d o;
+    multMatVec(v, &o, &projMat);
+    scaleVec(&o);
+    *v=o;
+  }
+  for (int i = 0; i<Cube.numFaces; i++) {
+    int ind[3];
+    ind[0] = Cube.faces[i].ind[0];
+    ind[1] = Cube.faces[i].ind[1];
+    ind[2] = Cube.faces[i].ind[2];
+    drawTriangle(&vertexes[ind[0]], &vertexes[ind[1]], &vertexes[ind[2]]);
+  }
+  free(vertexes);
 }
 
 void display() {
